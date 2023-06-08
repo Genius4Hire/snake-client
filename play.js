4cc88ed5271c5f0228e9c1bca7769f055bd12b83
Jordan Dennis <jordan.dennis@gmail.com>
2023-06-08 17:07:52 +0000

    Moved keyboard handler to input.js

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-const connect = require("./client");
+const {connect} = require("./client");
+const {setupInput} = require("./input");
 
-// set up standard in, so that we can interact with user input
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
 
-  stdin.on("data", (key) => {
-    switch (key) {
-      case 'w' : process.stdout.write("Wow!"); break;
-      case '\u0003' : process.exit(); break;
-      case 'x' : process.exit(); break;
-    
-      default : break;
-    }
-
-  })
-  console.log("After callback!")
-  return stdin;
-};
 
 
 console.log("Connecting ...");
