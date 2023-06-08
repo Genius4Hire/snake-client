4cc0206227e560e766749b7273f24e9a46f961d8
Jordan Dennis <jordan.dennis@gmail.com>
2023-06-08 17:49:41 +0000

    Added come thing to say, bound to keys:1,2,3,4,5, and 6

--- input.js
+++ input.js
@@ -18,6 +18,12 @@ const handleUserInput = function (key) {
     case 'a' : connection.write("Move: left"); break;
     case 's' : connection.write("Move: down"); break;
     case 'd' : connection.write("Move: right"); break;
+    case '1' : connection.write("Say: xLent"); break;
+    case '2' : connection.write("Say: move"); break;
+    case '3' : connection.write("Say: goof!"); break;
+    case '4' : connection.write("Say: foo"); break;
+    case '5' : connection.write("Say: bar"); break;
+    case '6' : connection.write("Say: Woot!"); break;
     // case '\u0003' : process.exit(); break; not working in ubuntu.. using 'x' to quit instead
     case 'x' : process.exit(); break;
     default : break;
