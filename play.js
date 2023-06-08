15b08e99e6b877898fe68bff7425ff270d6f7265
Jordan Dennis <jordan.dennis@gmail.com>
2023-06-08 16:22:21 +0000

    Added keyboard handler, and an 'exit' key, bound to 'x'

--- play.js
+++ play.js
@@ -1,5 +1,27 @@
-
 const connect = require("./client");
+
+// set up standard in, so that we can interact with user input
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", (key) => {
+    switch (key) {
+      case 'w' : process.stdout.write("Wow!"); break;
+      case '\u0003' : process.exit(); break;
+      case 'x' : process.exit(); break;
+    
+      default : break;
+    }
+
+  })
+  console.log("After callback!")
+  return stdin;
+};
+
 
 console.log("Connecting ...");
 connect();
+setupInput();
